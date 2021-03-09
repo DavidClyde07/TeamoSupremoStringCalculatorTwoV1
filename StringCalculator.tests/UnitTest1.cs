4a34a8427bc58c2c5d21a8c79e80d7d9cb2c8b70
DavidClyde07 <davidclydechetty@gmail.com>
2021-03-09 08:55:27 +0200

    Basic project structure.

---
+++ StringCalculator.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace StringCalculator.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
